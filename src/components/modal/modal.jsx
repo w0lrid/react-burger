7aff3a6a59e4a76a00e4eb9661c76686326f4f62
Dmitrii Kushnerev <w0lridem@gmail.com>
2023-04-06 23:41:07 +0300

    fix: upd closing modals

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -23,7 +23,7 @@ const Modal = ({children, active, handleClose}) => {
 
   return (
     <div className={`${styles.overlay} ${active && styles.overlayActive}`} onClick={handleClose}>
-      <div className={styles.modal}>
+      <div className={styles.modal} onClick={event => event.stopPropagation()}>
         <div className={styles.closeIcon}>
           <CloseIcon type="primary" onClick={handleClose}/>
         </div>
